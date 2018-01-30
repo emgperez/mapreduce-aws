b7d76383661ab17eda31cf576562687619be6570
emgperez <em.gonzalez.perez@gmail.com>
2018-01-30 19:41:51 +0100

    Java class for the reducer

--- HadoopHelloWorld/src/learning/bigdata/mapreduce/HitsByCountryReducer.java
+++ HadoopHelloWorld/src/learning/bigdata/mapreduce/HitsByCountryReducer.java
@@ -1,5 +1,34 @@
 package learning.bigdata.mapreduce;
 
-public class HitsByCountryReducer {
+import java.io.IOException;
+import java.util.Iterator;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class HitsByCountryReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	
+	private Text outputKey = new Text();
+	private IntWritable outputValue = new IntWritable();
+	private int count = 0;
+	
+	@Override
+	protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		
+		// Process every unique key emitted from the Mapper
+		count = 0;
+		Iterator<IntWritable> iter = values.iterator();
+		while (iter.hasNext()) {
+			IntWritable value = iter.next();
+			count += value.get();
+		}
+		
+		// Now let's set the output
+		outputKey.set(key);
+		outputValue.set(count);
+		context.write(outputKey, outputValue);
+		
+		
+	}
 }
